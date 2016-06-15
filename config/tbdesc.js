f2c4b03d3a79f35af20292f11191415e8caf17b0
FtLover <ftcaicai@gmail.com>
2016-06-16 00:01:54 +0800

    more view action

--- config/tbdesc.js
+++ config/tbdesc.js
@@ -1,19 +1,25 @@
 
  gTableDesc = {
      mail:{
+         name:"pay.notify",
+         countkey:"cpprivate",
+         orderkey:"transtime",
          kv:[
              {text:"玩家", dbkey:"cpprivate", class:"left aligned"},
-             {text:"金钱", dbkey:"money"},
+             {text:"金钱", dbkey:"money", headtag:'<i class="yellow icon dollar"></i>'},
              {text:"时间", dbkey:"transtime"},
          ],
      },
      character:{
+         name:"as_game.tb_character",
+         countkey:"charnid",
+         orderkey:"level",
          kv:[
              {text:"ID", dbkey:"charnid"},
-             {text:"角色名", dbkey:"charsid"},
+             {text:"角色名", dbkey:"charsid", filter:"string"},
              {text:"职业", dbkey:"class"},
-             {text:"等级", dbkey:"level"},
-             {text:"最后上线时间", dbkey:"LastConnTime"},
+             {text:"等级", dbkey:"level", filter:"int"},
+             {text:"最后上线时间", dbkey:"LastConnTime", type:"time"},
          ],
      }
  }
--- public/javascripts/ark.js
+++ public/javascripts/ark.js
@@ -14,4 +14,32 @@ $(function () {
             action: 'combo'
         })
     ;
+    $('.dropdown.button')
+        .dropdown({on:'click'});
+
+    $('.sortable.table').tablesort();
+
+    $('#viewSearch').click(function(){
+        //post('/users',{page:2})
+    })
 });
+
+function post(URL, PARAMS) {
+    var temp = document.createElement("form");
+    temp.action = URL;
+    temp.method = "post";
+    temp.style.display = "none";
+    for (var x in PARAMS) {
+        var opt = document.createElement("textarea");
+        opt.name = x;
+        opt.value = PARAMS[x];
+        // alert(opt.name)
+        temp.appendChild(opt);
+    }
+    document.body.appendChild(temp);
+    temp.submit();
+    return temp;
+}
+
+//调用方法 如
+//post('pages/statisticsJsp/excel.action', {html :prnhtml,cm1:'sdsddsd',cm2:'haha'});
